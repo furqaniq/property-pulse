0051bee1c5efa939cc5ae54ac9e4b6441fdf41f5
furqaniq <furqan93@gmail.com>
2024-11-23 01:28:54 +0500

    module-8 Property Search

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropertyCard from '@/components/PropertyCard';
 import { fetchProperties } from '@/utils/requests';
+import PropertySearchForm from '@/components/PropertySearchForm';
 
 
 const PropertiesPage = async () => {
@@ -10,6 +11,14 @@ const PropertiesPage = async () => {
   properties.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
 
   return (
+
+    <>
+    <section className='bg-blue-700 py-4'>
+        <div className='max-w-7xl mx-auto px-4 flex flex-col items-start sm:px-6 lg:px-8'>
+           <PropertySearchForm />
+        </div>
+     </section>
+
     <section className="px-4 py-6">
       <div className="container-xl lg:container m-auto px-4 py-6">
         { properties.length === 0 ? (
@@ -22,7 +31,9 @@ const PropertiesPage = async () => {
         </div>
         ) }
        </div>
-    </section>      
+    </section>
+    </>
+          
   )
 }
 
