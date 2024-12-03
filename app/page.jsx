11658b7017bc9ae531e05b6a8c1ab4adc4411e19
furqaniq <furqan93@gmail.com>
2024-12-04 04:12:37 +0500

    in page.jsx export const dynamic = 'force-dynamic';

--- app/page.jsx
+++ app/page.jsx
@@ -4,6 +4,7 @@ import InfoBoxes from '@/components/InfoBoxes';
 import HomeProperties from '@/components/HomeProperties';
 import FeaturedProperties from '@/components/FeaturedProperties';
 
+export const dynamic = 'force-dynamic';
 
 const HomePage =  () => {
   return (
